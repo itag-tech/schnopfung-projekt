51f94f163c26ce61f1710423cbc7b43bf1e27109
Thomas AGUIRREGABIRIA <hello@itag-technology.com>
2021-09-09 12:51:25 +0200

    Page produit avec carousel + bouton panier

---
+++ src/components/layout/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Component from './layout'
+
+const Layout = ({children}) => {
+    return (
+        <Component children={children}/>
+    )
+}
+
+export default Layout
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,25 +3,37 @@ import { Link, graphql,  } from 'gatsby'
 import { GatsbyImage } from 'gatsby-plugin-image'
 import { Card, Icon, Spin } from 'antd'
 import { SnipcartContext } from 'gatsby-plugin-snipcart-advanced/context'
-import Layout from '../components/layout'
+import Layout from '../components/layout/'
 import Seo from '../components/seo'
 import 'antd/dist/antd.css'
+import './index.scss'
 
 const { Meta } = Card;
 
+/**
+ * 
+ * @param {data} Données provenant d'une requêtes GraphQL ci-dessous (= DatoCMS).
+ * @returns 
+ */
 const IndexPage = ({data}) => {
 
   const [products, setProduct] = useState()
-  const snipcartContext = useContext(SnipcartContext);
+  const snipcartContext = useContext(SnipcartContext); // snipcart context.
   
   useEffect(() => {
-    setProduct(data.allDatoCmsProduct.edges)
+    let datoCmsProducts = data.allDatoCmsProduct.edges // liste de produits de DatoCMS
+    setProduct(datoCmsProducts)
   }, [data])
   
-  if(!products) {
-    return(<Spin size="large" />)
+  if(!products && snipcartContext) {
+    return(
+      <div className='spinner'>
+        <Spin size="large" />
+      </div>
+      )
   } else {
     return (
+      // Injection du context dans les éléments enfants du Provider.
       <SnipcartContext.Provider value={snipcartContext}>
         <Layout>
           <Seo title="Accueil" />
